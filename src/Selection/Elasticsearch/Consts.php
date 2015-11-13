ddc312b45295416b04e94af47bcbbaaa61a47a8d
Drasko Gomboc <drasko.gomboc@gmail.com>
2015-11-13 12:05:18 +0100

    Elastic selection consts

---
+++ src/Selection/Elasticsearch/Consts.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace G4\DataMapper\Selection\Elasticsearch;
+
+class Consts
+{
+
+    const MUST     = 'must';
+
+    const MUST_NOT = 'must_not';
+
+    const TERM     = 'term';
+
+    const TERMS    = 'terms';
+
+    const WILDCARD = 'wildcard';
+
+    const WILDCARD_POSITION_BOTH  = 'WILDCARD_POSITION_BOTH';
+
+    const WILDCARD_POSITION_LEFT  = 'WILDCARD_POSITION_LEFT';
+
+    const WILDCARD_POSITION_RIGHT = 'WILDCARD_POSITION_RIGHT';
+}
