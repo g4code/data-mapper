b7af98d5dab85990f53227425f4708d83b5ad428
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-03-02 10:20:46 +0100

    Added elasticsearch recipe

--- cookbooks/engine/metadata.rb
+++ cookbooks/engine/metadata.rb
@@ -2,3 +2,5 @@ name 'engine'
 
 depends 'apt'
 depends 'mysql', '~> 6.0'
+depends 'java', '~> 1.39.0'
+depends 'elasticsearch', '~> 2.2.0'
--- cookbooks/engine/recipes/default.rb
+++ cookbooks/engine/recipes/default.rb
@@ -16,3 +16,26 @@ bash 'grant_privileges' do
     mysql -h 127.0.0.1 -u root -proot -e "FLUSH PRIVILEGES;"
     EOH
 end
+
+node.override[:java][:openjdk_packages] = ["openjdk-7-jdk"]
+include_recipe 'java'
+
+elasticsearch_user 'elasticsearch' do
+  action :nothing
+end
+
+elasticsearch_install 'elasticsearch' do
+  type :package
+end
+
+elasticsearch_configure 'elasticsearch' do
+    allocated_memory '256m'
+    configuration ({
+      'cluster.name' => 'data_mapper',
+      'node.name' => 'node01'
+    })
+end
+
+elasticsearch_service 'elasticsearch' do
+  service_actions [:enable, :start]
+end
