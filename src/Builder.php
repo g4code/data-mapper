f0bfdaaa4f1de6496d6ee8766003e921b6671558
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-04-04 18:20:41 +0200

    Added buildBulk in Builder class

--- src/Builder.php
+++ src/Builder.php
@@ -2,6 +2,7 @@
 
 namespace G4\DataMapper;
 
+use G4\DataMapper\Common\Bulk;
 use G4\DataMapper\Engine\MySQL\MySQLClientFactory;
 use G4\DataMapper\Common\AdapterInterface;
 use G4\DataMapper\Common\MapperInterface;
@@ -64,6 +65,20 @@ class Builder
         }
 
         return $this->strategy();
+    }
+
+    //TODO: Drasko - refactor this!
+    public function buildBulk()
+    {
+        if (!$this->adapter instanceof AdapterInterface) {
+            throw new \Exception('Adapter instance must implement AdapterInterface', 601);
+        }
+
+        if ($this->dataSet === null) {
+            throw new \Exception('DataSet cannot be emty', 601);
+        }
+
+        return new Bulk($this->adapter, $this->dataSet);
     }
 
     /**
