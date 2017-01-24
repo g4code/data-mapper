e13c916332e32eaf44c40457c3c7d41eb56c1612
Sinisa Lackovic <sinisalackovic@yahoo.com>
2017-01-24 07:39:49 +0100

    Modified the MySQLClientFactory filterParams method to throw an exception only if a password parameter is isset and is null

--- src/Engine/MySQL/MySQLClientFactory.php
+++ src/Engine/MySQL/MySQLClientFactory.php
@@ -50,7 +50,7 @@ class MySQLClientFactory implements CreateInterface
         if (empty($params['username'])) {
             throw new \Exception('No username param', 101);
         }
-        if (empty($params['password'])) {
+        if(isset($params['password']) && is_null($params['password'])){
             throw new \Exception('No password param', 101);
         }
         if (empty($params['dbname'])) {
