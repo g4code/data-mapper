e176770590276fe93dd4f6b002302000485e3b3c
Vladan <vladan.zoricic@gmail.com>
2017-12-11 13:08:43 +0100

    000 - Added skeleton for Solr Comparison Formatter class.

---
+++ src/Engine/Solr/SolrComparisonFormatter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace G4\DataMapper\Engine\Solr;
+
+use G4\DataMapper\Common\ComparisonFormatterInterface;
+use G4\DataMapper\Common\Selection\Operator;
+
+class SolrComparisonFormatter implements ComparisonFormatterInterface
+{
+
+    public function format($name, Operator $operator, $value)
+    {
+        
+    }
+}
