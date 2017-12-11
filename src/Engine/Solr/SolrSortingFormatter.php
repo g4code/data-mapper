287b246edbd3e931559310207bd065c1b8953d28
Vladan <vladan.zoricic@gmail.com>
2017-12-11 13:06:26 +0100

    000 - Added Solr Sorting Formatter class .

---
+++ src/Engine/Solr/SolrSortingFormatter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace G4\DataMapper\Engine\Solr;
+
+use G4\DataMapper\Common\SortingFormatterInterface;
+use G4\DataMapper\Common\Selection\Sort;
+
+class SolrSortingFormatter implements SortingFormatterInterface
+{
+    private $map = [
+        Sort::ASCENDING => 'asc',
+        Sort::DESCENDING => 'desc',
+    ];
+    
+    public function format($name, $order)
+    {
+        return sprintf("%s %s", $name, $this->sortMap($order));
+    }
+
+    private function sortMap($order)
+    {
+        if(!isset($this->map[$order])) {
+            throw new \Exception('Order is not in map', 101);
+        }
+
+        return $this->map[$order];
+    }
+}
