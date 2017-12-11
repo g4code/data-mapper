11f165fe7f08c8a4abaedfa398b8572fa367da89
Vladan <vladan.zoricic@gmail.com>
2017-12-11 15:33:04 +0100

    000 - Added logic for equal operator.

--- src/Engine/Solr/SolrComparisonFormatter.php
+++ src/Engine/Solr/SolrComparisonFormatter.php
@@ -7,9 +7,24 @@ use G4\DataMapper\Common\Selection\Operator;
 
 class SolrComparisonFormatter implements ComparisonFormatterInterface
 {
+    private $map = [
+        Operator::EQUAL => ':',
+    ];
 
     public function format($name, Operator $operator, $value)
     {
-
+        return sprintf("%s%s%s", $name, $this->operatorMap($operator), $value);
     }
+
+    private function operatorMap(Operator $operator)
+    {
+        $symbol = $operator->getSymbol();
+
+        if (!isset($this->map[$symbol])) {
+            throw new \Exception('Operator not im map', 101);
+        }
+
+        return $this->map[$symbol];
+    }
+
 }
