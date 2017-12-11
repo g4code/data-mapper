6bb82872cf8a11342fe8abac8aeb8f78d0dc5336
Vladan <vladan.zoricic@gmail.com>
2017-12-11 15:55:01 +0100

    000 - Removed map private attribute, removed unnecessary operatorMap private method, added logic for greather than operation.

--- src/Engine/Solr/SolrComparisonFormatter.php
+++ src/Engine/Solr/SolrComparisonFormatter.php
@@ -7,24 +7,13 @@ use G4\DataMapper\Common\Selection\Operator;
 
 class SolrComparisonFormatter implements ComparisonFormatterInterface
 {
-    private $map = [
-        Operator::EQUAL => ':',
-    ];
 
     public function format($name, Operator $operator, $value)
     {
-        return sprintf("%s%s%s", $name, $this->operatorMap($operator), $value);
-    }
-
-    private function operatorMap(Operator $operator)
-    {
-        $symbol = $operator->getSymbol();
-
-        if (!isset($this->map[$symbol])) {
-            throw new \Exception('Operator not im map', 101);
+        if($operator->getSymbol() === Operator::EQUAL) {
+            return $name . ':' . $value;
+        } elseif ($operator->getSymbol() === Operator::GRATER_THAN) {
+            return $name . ':' . '{' . $value . ' TO ' . '*' . '}';
         }
-
-        return $this->map[$symbol];
     }
-
 }
