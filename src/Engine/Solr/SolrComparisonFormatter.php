a21e62fc8ee4abda5026530b06cd81cdaf44fec2
Vladan <vladan.zoricic@gmail.com>
2017-12-12 11:11:55 +0100

    000 - Added additional constants, added logic for in operation.

--- src/Engine/Solr/SolrComparisonFormatter.php
+++ src/Engine/Solr/SolrComparisonFormatter.php
@@ -7,17 +7,23 @@ use G4\DataMapper\Common\Selection\Operator;
 
 class SolrComparisonFormatter implements ComparisonFormatterInterface
 {
-    const BRACKET_CLOSE       = ']';
-    const BRACKET_OPEN        = '[';
+    const ROUND_BRACKET_OPEN   = '(';
+    const ROUND_BRACKET_CLOSE  = ')';
 
-    const CURLY_BRACKET_CLOSE = '}';
-    const CURLY_BRACKET_OPEN  = '{';
+    const SQUARE_BRACKET_CLOSE = ']';
+    const SQUARE_BRACKET_OPEN  = '[';
 
-    const COLON               = ':';
-    const TO                  = 'TO';
-    const WILDCARD            = '*';
+    const CURLY_BRACKET_CLOSE  = '}';
+    const CURLY_BRACKET_OPEN   = '{';
 
-    const EMPTY_SPACE         = ' ';
+    const COLON                = ':';
+    const WILDCARD             = '*';
+
+    const CONNECTOR_TO         = 'TO';
+    const CONNECTOR_OR         = 'OR';
+
+    const EMPTY_SPACE          = ' ';
+    const COMMA                = ',';
 
 
     public function format($name, Operator $operator, $value)
@@ -25,13 +31,15 @@ class SolrComparisonFormatter implements ComparisonFormatterInterface
         if($operator->getSymbol() === Operator::EQUAL) {
             return $name . self::COLON . $value;
         } elseif ($operator->getSymbol() === Operator::GRATER_THAN) {
-            return $name . self::COLON . self::CURLY_BRACKET_OPEN . $value . self::EMPTY_SPACE . self::TO . self::EMPTY_SPACE . self::WILDCARD . self::CURLY_BRACKET_CLOSE;
+            return $name . self::COLON . self::CURLY_BRACKET_OPEN . $value . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . self::WILDCARD . self::CURLY_BRACKET_CLOSE;
         } elseif ($operator->getSymbol() === Operator::LESS_THAN) {
-            return $name . self::COLON . self::CURLY_BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::TO . self::EMPTY_SPACE . $value . self::CURLY_BRACKET_CLOSE;
+            return $name . self::COLON . self::CURLY_BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . $value . self::CURLY_BRACKET_CLOSE;
         } elseif ($operator->getSymbol() === Operator::GRATER_THAN_OR_EQUAL) {
-            return $name . self::COLON . self::BRACKET_OPEN . $value . self::EMPTY_SPACE . self::TO . self::EMPTY_SPACE . self::WILDCARD . self::BRACKET_CLOSE;
+            return $name . self::COLON . self::SQUARE_BRACKET_OPEN . $value . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . self::WILDCARD . self::SQUARE_BRACKET_CLOSE;
         } elseif ($operator->getSymbol() === Operator::LESS_THAN_OR_EQUAL) {
-            return $name . self::COLON . self::BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::TO . self::EMPTY_SPACE . $value . self::BRACKET_CLOSE;
+            return $name . self::COLON . self::SQUARE_BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . $value . self::SQUARE_BRACKET_CLOSE;
+        } elseif ($operator->getSymbol() === Operator::IN) {
+            return $name . self::COLON . self::ROUND_BRACKET_OPEN . str_replace(self::COMMA, self::EMPTY_SPACE . self::CONNECTOR_OR, $value) .self::ROUND_BRACKET_CLOSE;
         }
     }
 }
