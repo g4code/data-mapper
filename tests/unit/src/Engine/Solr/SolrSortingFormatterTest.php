8d9581523e7fff942a7fda35b398aca6f1498fa4
Vladan <vladan.zoricic@gmail.com>
2017-12-12 14:33:53 +0100

    000 - Added initial skeleton .

---
+++ tests/unit/src/Engine/Solr/SolrSortingFormatterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use G4\DataMapper\Engine\Solr\SolrSortingFormatter;
+use G4\DataMapper\Common\Selection\Sort;
+
+class SolrSortingFormatterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SolrSortingFormatter
+     */
+    private $sortingFormatter;
+
+    protected function setUp()
+    {
+        $this->sortingFormatter = new SolrSortingFormatter();
+    }
+
+    protected function tearDown()
+    {
+        $this->sortingFormatter = null;
+    }
+}
