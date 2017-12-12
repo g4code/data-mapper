6c4461eea32ca813e9987c2f41c56e584cdd0a4b
Vladan <vladan.zoricic@gmail.com>
2017-12-12 12:06:50 +0100

    000 - Query commands extracted into separate methods.

--- src/Engine/Solr/SolrComparisonFormatter.php
+++ src/Engine/Solr/SolrComparisonFormatter.php
@@ -29,17 +29,85 @@ class SolrComparisonFormatter implements ComparisonFormatterInterface
     public function format($name, Operator $operator, $value)
     {
         if($operator->getSymbol() === Operator::EQUAL) {
-            return $name . self::COLON . $value;
+            return $this->formatEqualQuery($name, $value);
         } elseif ($operator->getSymbol() === Operator::GRATER_THAN) {
-            return $name . self::COLON . self::CURLY_BRACKET_OPEN . $value . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . self::WILDCARD . self::CURLY_BRACKET_CLOSE;
+            return $this->formatGreaterThanQuery($name, $value);
         } elseif ($operator->getSymbol() === Operator::LESS_THAN) {
-            return $name . self::COLON . self::CURLY_BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . $value . self::CURLY_BRACKET_CLOSE;
+            return $this->formatLessThanQuery($name, $value);
         } elseif ($operator->getSymbol() === Operator::GRATER_THAN_OR_EQUAL) {
-            return $name . self::COLON . self::SQUARE_BRACKET_OPEN . $value . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . self::WILDCARD . self::SQUARE_BRACKET_CLOSE;
+            return $this->formatGreaterThanOrEqualQuery($name, $value);
         } elseif ($operator->getSymbol() === Operator::LESS_THAN_OR_EQUAL) {
-            return $name . self::COLON . self::SQUARE_BRACKET_OPEN . self::WILDCARD . self::EMPTY_SPACE . self::CONNECTOR_TO . self::EMPTY_SPACE . $value . self::SQUARE_BRACKET_CLOSE;
+            return $this->formatLessThanOrEqualQuery($name, $value);
         } elseif ($operator->getSymbol() === Operator::IN) {
-            return $name . self::COLON . self::ROUND_BRACKET_OPEN . str_replace(self::COMMA, self::EMPTY_SPACE . self::CONNECTOR_OR, $value) .self::ROUND_BRACKET_CLOSE;
+            return $this->formatInQuery($name, $value);
         }
+    }
+
+    private function formatEqualQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . $value;
+    }
+
+    private function formatGreaterThanQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . self::CURLY_BRACKET_OPEN
+            . $value
+            . self::EMPTY_SPACE
+            . self::CONNECTOR_TO
+            . self::EMPTY_SPACE
+            . self::WILDCARD
+            . self::CURLY_BRACKET_CLOSE;
+    }
+
+    private function formatLessThanQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . self::CURLY_BRACKET_OPEN
+            . self::WILDCARD
+            . self::EMPTY_SPACE
+            . self::CONNECTOR_TO
+            . self::EMPTY_SPACE
+            . $value
+            . self::CURLY_BRACKET_CLOSE;
+    }
+
+    private function formatGreaterThanOrEqualQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . self::SQUARE_BRACKET_OPEN
+            . $value
+            . self::EMPTY_SPACE
+            . self::CONNECTOR_TO
+            . self::EMPTY_SPACE
+            . self::WILDCARD
+            . self::SQUARE_BRACKET_CLOSE;
+    }
+
+    private function formatLessThanOrEqualQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . self::SQUARE_BRACKET_OPEN
+            . self::WILDCARD
+            . self::EMPTY_SPACE
+            . self::CONNECTOR_TO
+            . self::EMPTY_SPACE
+            . $value
+            . self::SQUARE_BRACKET_CLOSE;
+    }
+
+    private function formatInQuery($name, $value)
+    {
+        return $name
+            . self::COLON
+            . self::ROUND_BRACKET_OPEN
+            . str_replace(self::COMMA, self::EMPTY_SPACE . self::CONNECTOR_OR, $value)
+            . self::ROUND_BRACKET_CLOSE;
     }
 }
