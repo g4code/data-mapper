509f40d41a895be726d9e3a537fdcc4e9693a615
Vladan <vladan.zoricic@gmail.com>
2017-12-12 11:11:02 +0100

    000 - Added unit test for in operator in Solr Comparison Formatter class.

--- tests/unit/src/Engine/Solr/SolrComparisonFormatterTest.php
+++ tests/unit/src/Engine/Solr/SolrComparisonFormatterTest.php
@@ -69,4 +69,13 @@ class SolrComparisonFormatterTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('age:[* TO 18]', $this->comparisonFormatter->format('age', $this->operatorMock, '18'));
     }
+
+    public function testIn()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::IN);
+
+        $this->assertEquals('age:(18 OR 19 OR 20)', $this->comparisonFormatter->format('age', $this->operatorMock, '18, 19, 20'));
+    }
 }
