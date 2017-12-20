03ce78ba977e35db21bfe81ab50d833c9700240f
Vladan <vladan.zoricic@gmail.com>
2017-12-20 11:42:33 +0100

    000 - Added new error code

--- src/ErrorCodes.php
+++ src/ErrorCodes.php
@@ -7,6 +7,7 @@ class ErrorCodes
     const DATA_MAPPER_ERROR          = 101;
     const ORDER_NOT_IN_MAP           = 102;
     const NO_HOST_PARAMETER          = 103;
+    const NO_PORT_PARAMETER          = 104;
     const TABLE_NAME_NOT_STRING      = 4001;
     const COLLECTION_NAME_NOT_STRING = 4005;
 }
