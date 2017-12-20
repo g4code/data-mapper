9ca145b68efd90f6c4e0839504903d12f26017f5
Vladan <vladan.zoricic@gmail.com>
2017-12-20 14:53:33 +0100

    000 - Changed indentation.

--- src/ErrorMessages.php
+++ src/ErrorMessages.php
@@ -4,8 +4,8 @@ namespace G4\DataMapper;
 
 class ErrorMessages
 {
-    const ORDER_NOT_IN_MAP           = 'Order is not in map.';
-    const NO_HOST_PARAMETER          = 'No host parameter.';
-    const NO_PORT_PARAMETER          = 'No port parameter.';
-    const EMPTY_DATA                 = 'Empty data.';
+    const ORDER_NOT_IN_MAP  = 'Order is not in map.';
+    const NO_HOST_PARAMETER = 'No host parameter.';
+    const NO_PORT_PARAMETER = 'No port parameter.';
+    const EMPTY_DATA        = 'Empty data.';
 }
