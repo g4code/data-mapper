2f966e7920e768cfae29dc6f95d3ccc6ab5fd471
Vladan <vladan.zoricic@gmail.com>
2017-12-20 15:00:17 +0100

    000 - Added error message constant instead of hardcoded message.

--- src/Exception/EmptyDataException.php
+++ src/Exception/EmptyDataException.php
@@ -3,13 +3,14 @@
 namespace G4\DataMapper\Exception;
 
 use G4\DataMapper\ErrorCodes as ErrorCode;
+use G4\DataMapper\ErrorMessages as ErrorMessage;
 
 class EmptyDataException extends \Exception
 {
     public function __construct($message = '')
     {
         if(!$message) {
-            $message = 'Empty data.';
+            $message = ErrorMessage::EMPTY_DATA;
         }
 
         parent::__construct($message, ErrorCode::EMPTY_DATA);
