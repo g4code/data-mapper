34902faf1e89c0c818ccfc501fc8d7a452b34c5b
Vladan <vladan.zoricic@gmail.com>
2018-01-09 14:33:59 +0100

    000 - Added initial skeleton for Elastic Search adapter class.

---
+++ src/Engine/Elasticsearch/ElasticsearchAdapter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace G4\DataMapper\Engine\Elasticsearch;
+
+use G4\DataMapper\Common\AdapterInterface;
+use G4\DataMapper\Common\CollectionNameInterface;
+use G4\DataMapper\Common\MappingInterface;
+use G4\DataMapper\Common\RawData;
+use G4\DataMapper\Common\SelectionFactoryInterface;
+use G4\DataMapper\Exception\EmptyDataException;
+
+class ElasticsearchAdapter implements AdapterInterface
+{
+    private $client;
+
+    public function __construct(ElasticsearchClientFactory $clientFactory)
+    {
+        $this->client = $clientFactory->create();
+    }
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param SelectionFactoryInterface $selectionFactory
+     */
+    public function delete(CollectionNameInterface $collectionName, SelectionFactoryInterface $selectionFactory)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param MappingInterface $mapping
+     */
+    public function insert(CollectionNameInterface $collectionName, MappingInterface $mapping)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param \ArrayIterator $mappingsCollection
+     */
+    public function insertBulk(CollectionNameInterface $collectionName, \ArrayIterator $mappingsCollection)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param \ArrayIterator $mappingsCollection
+     */
+    public function upsertBulk(CollectionNameInterface $collectionName, \ArrayIterator $mappingsCollection)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param SelectionFactoryInterface $selectionFactory
+     * @return RawData
+     */
+    public function select(CollectionNameInterface $collectionName, SelectionFactoryInterface $selectionFactory)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param MappingInterface $mapping
+     * @param SelectionFactoryInterface $selectionFactory
+     */
+    public function update(CollectionNameInterface $collectionName, MappingInterface $mapping, SelectionFactoryInterface $selectionFactory = null)
+    {}
+
+    /**
+     * @param CollectionNameInterface $collectionName
+     * @param MappingInterface $mapping
+     */
+    public function upsert(CollectionNameInterface $collectionName, MappingInterface $mapping)
+    {}
+
+    /**
+     * @param string $query
+     * @return mixed
+     */
+    public function query($query)
+    {}
+}
