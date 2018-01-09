9f8cbff63c7bb37dc5a31425e02694b4f44b03b5
Vladan <vladan.zoricic@gmail.com>
2018-01-09 14:26:03 +0100

    000 - Added initial functionality for Elastic Search client class.

--- src/Engine/Elasticsearch/ElasticsearchClient.php
+++ src/Engine/Elasticsearch/ElasticsearchClient.php
@@ -6,11 +6,69 @@ use G4\ValueObject\Url;
 
 class ElasticsearchClient
 {
+    const DOCUMENT      = 'doc';
+    const TIMEOUT       = 5;
+    const METHOD_POST   = 'POST';
+    const METHOD_PUT    = 'PUT';
+    const METHOD_GET    = 'GET';
+    const METHOD_DELETE = 'DELETE';
 
+    /**
+     * @var Url
+     */
     private $url;
 
-    public function __construct(Url $url)
+    private $query;
+
+
+    public function __construct(Url $url, $index)
     {
-        $this->url = $url;
+        $this->url = $url->path($index, self::DOCUMENT);
+    }
+
+    public function insert()
+    {
+        $this->execute(self::METHOD_POST);
+    }
+
+    public function update()
+    {
+        $this->execute(self::METHOD_PUT);
+    }
+
+    public function get()
+    {
+        $this->execute(self::METHOD_GET);
+    }
+
+    public function delete()
+    {
+        $this->execute(self::METHOD_DELETE);
+    }
+
+    public function setQuery($value)
+    {
+        $this->query = $value;
+        return $this;
+    }
+
+    private function execute($method)
+    {
+        $handle = curl_init((string) $this->url);
+
+        curl_setopt_array($handle, [
+            CURLOPT_POSTFIELDS     => json_encode($this->query),
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_TIMEOUT        => self::TIMEOUT,
+            CURLOPT_URL            => (string) $this->url,
+            CURLOPT_HTTPHEADER     => ['Content-Type: application/json'],
+            CURLOPT_CUSTOMREQUEST  => $method,
+        ]);
+
+        $this->response = curl_exec($handle);
+
+        curl_close($handle);
+
+        return $this;
     }
 }
