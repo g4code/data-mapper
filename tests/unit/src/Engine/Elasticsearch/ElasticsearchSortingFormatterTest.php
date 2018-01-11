3a206b3d85cf3c1f2e4a9d9d84d76facf8f28598
Vladan <vladan.zoricic@gmail.com>
2018-01-11 10:47:24 +0100

    000 - Added unit tests for Elastic Seatch sorting formatter class.

---
+++ tests/unit/src/Engine/Elasticsearch/ElasticsearchSortingFormatterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use G4\DataMapper\Engine\Elasticsearch\ElasticsearchSortingFormatter;
+use G4\DataMapper\Common\Selection\Sort;
+use G4\DataMapper\Exception\OrderNotInMapException;
+use G4\DataMapper\ErrorCodes as ErrorCode;
+use G4\DataMapper\ErrorMessages as ErrorMessage;
+
+class ElasticsearchSortingFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ElasticsearchSortingFormatter
+     */
+    private $sortingFormatter;
+
+    protected function setUp()
+    {
+        $this->sortingFormatter = new ElasticsearchSortingFormatter();
+    }
+
+    protected function tearDown()
+    {
+        $this->sortingFormatter = null;
+    }
+
+    public function testAscendingFormat()
+    {
+        $this->assertEquals('name:asc', $this->sortingFormatter->format('name', Sort::ASCENDING));
+    }
+
+    public function testDescendingFormat()
+    {
+        $this->assertEquals('name:desc', $this->sortingFormatter->format('name', Sort::DESCENDING));
+    }
+
+    public function testOrderNotInMap()
+    {
+        $this->expectException(OrderNotInMapException::class);
+        $this->expectExceptionMessage(ErrorMessage::ORDER_NOT_IN_MAP);
+        $this->expectExceptionCode(ErrorCode::ORDER_NOT_IN_MAP);
+
+        $this->sortingFormatter->format('name', 'test');
+    }
+}
