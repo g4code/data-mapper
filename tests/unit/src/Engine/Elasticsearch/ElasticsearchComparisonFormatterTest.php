4cb8ef0d805c4b1757790d2f34d073902b0b829e
Vladan <vladan.zoricic@gmail.com>
2018-01-11 15:09:36 +0100

    000 - Added unit tests for Elastic search comparison formatter class.

---
+++ tests/unit/src/Engine/Elasticsearch/ElasticsearchComparisonFormatterTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use G4\DataMapper\Engine\Elasticsearch\ElasticsearchComparisonFormatter;
+use G4\DataMapper\Common\Selection\Operator;
+
+class ElasticsearchComparisonFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ElasticsearchComparisonFormatter
+     */
+    private $comparisonFormatter;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $operatorMock;
+
+    protected function setUp()
+    {
+        $this->comparisonFormatter = new ElasticsearchComparisonFormatter();
+
+        $this->operatorMock = $this->getMockBuilder(\G4\DataMapper\Common\Selection\Operator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function tearDown()
+    {
+        $this->comparisonFormatter = null;
+        $this->operatorMock        = null;
+    }
+
+    public function testEqual()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::EQUAL);
+
+        $this->assertEquals(['match' => ['age' => '15']], $this->comparisonFormatter->format('age', $this->operatorMock, '15'));
+    }
+
+    public function testGreaterThan()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::GRATER_THAN);
+
+        $this->assertEquals(['range' => ['age' => ['gt' => '18']]], $this->comparisonFormatter->format('age', $this->operatorMock, '18'));
+    }
+
+    public function testLessThan()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::LESS_THAN);
+
+        $this->assertEquals(['range' => ['age' => ['lt' => '18']]], $this->comparisonFormatter->format('age', $this->operatorMock, '18'));
+    }
+
+    public function testGreaterThanOrEqual()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::GRATER_THAN_OR_EQUAL);
+
+        $this->assertEquals(['range' => ['age' => ['gte' => '18']]], $this->comparisonFormatter->format('age', $this->operatorMock, '18'));
+    }
+
+    public function testLessThanOrEqual()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::LESS_THAN_OR_EQUAL);
+
+        $this->assertEquals(['range' => ['age' => ['lte' => '18']]], $this->comparisonFormatter->format('age', $this->operatorMock, '18'));
+    }
+
+    public function testIn()
+    {
+        $this->operatorMock->expects($this->any())
+            ->method('getSymbol')
+            ->willReturn(Operator::IN);
+
+        $this->assertEquals(['terms' => ['age' => [18, 19, 20]]], $this->comparisonFormatter->format('age', $this->operatorMock, '18, 19, 20'));
+    }
+}
