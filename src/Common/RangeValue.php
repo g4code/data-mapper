70f903df363021aca57262632782e716975d06c3
Vladan <vladan.zoricic@gmail.com>
2018-01-17 13:17:21 +0100

    000 - Added RangeValue value object.

---
+++ src/Common/RangeValue.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace G4\DataMapper\Common;
+
+class RangeValue
+{
+    /**
+     * @var string
+     */
+    private $min;
+
+    /**
+     * @var string
+     */
+    private $max;
+
+    public function __construct($min, $max)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    public function getMin()
+    {
+        return min($this->min, $this->max);
+    }
+
+    public function getMax()
+    {
+        return max($this->min, $this->max);
+    }
+}
