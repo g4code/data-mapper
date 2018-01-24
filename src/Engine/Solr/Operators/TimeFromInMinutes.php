39be5a15a5acec23db6cd1ebb9fbf5a799fd49e2
Vladan <vladan.zoricic@gmail.com>
2018-01-24 09:42:22 +0100

    000 - Added new TimeFromInMinutes operator.

---
+++ src/Engine/Solr/Operators/TimeFromInMinutes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace G4\DataMapper\Engine\Solr\Operators;
+
+use G4\DataMapper\Common\SingleValue;
+use G4\DataMapper\Common\QueryConnector;
+
+class TimeFromInMinutes implements QueryOperatorInterface
+{
+    private $name;
+
+    private $value;
+
+    public function __construct($name, SingleValue $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function format()
+    {
+        return $this->name
+            . QueryConnector::COLON
+            . QueryConnector::SQUARE_BRACKET_OPEN
+            . "NOW-#{$this->value}MINUTES"
+            . QueryConnector::EMPTY_SPACE
+            . QueryConnector::CONNECTOR_TO
+            . QueryConnector::EMPTY_SPACE
+            . QueryConnector::WILDCARD
+            . QueryConnector::SQUARE_BRACKET_CLOSE;
+    }
+}
