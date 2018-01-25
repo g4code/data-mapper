bc9e3d8d50ff9bc97fcf0a08fb257d819bbab0f6
Vladan <vladan.zoricic@gmail.com>
2018-01-25 14:40:36 +0100

    000 - Added geodist method.

--- src/Engine/Solr/SolrIdentity.php
+++ src/Engine/Solr/SolrIdentity.php
@@ -3,23 +3,68 @@
 namespace G4\DataMapper\Engine\Solr;
 
 use G4\DataMapper\Common\Identity;
+use G4\DataMapper\Common\Selection\Operator;
+use G4\DataMapper\Common\SingleValue;
+use G4\DataMapper\Common\CoordinatesValue;
 
 class SolrIdentity extends Identity implements SolrIdentityInterface
 {
+
+    /**
+     * @var string
+     */
     private $rawQuery;
 
+    /**
+     * @param $value
+     */
     public function setRawQuery($value)
     {
         $this->rawQuery = $value;
     }
 
+    /**
+     * @return string
+     */
     public function getRawQuery()
     {
-        $this->rawQuery;
+        return $this->rawQuery;
     }
 
+    /**
+     * @return bool
+     */
     public function hasRawQuery()
     {
         return !empty($this->rawQuery);
+    }
+
+    /**
+     * @param string $value
+     * @return SolrIdentity
+     */
+    public function timeFromInMinutes($value)
+    {
+        $this->arrayException($value);
+        $this->operator(Operator::TIME_FROM_IN_MINUTES, new SingleValue($value));
+        return $this;
+    }
+
+    /**
+     * @param $latitude
+     * @param $longitude
+     * @param $distance
+     * @return SolrIdentity
+     */
+    public function geodist($latitude, $longitude, $distance)
+    {
+        $coordinates = new CoordinatesValue($latitude, $longitude);
+
+        if (isset($distance)) {
+            $coordinates->setDistance($distance);
+        }
+
+        $this->operator(Operator::GEODIST, $coordinates);
+        return $this;
     }
 }
