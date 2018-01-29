cbe8796673f55cfdd5406acbd4f99ca4a38e9848
Vladan <vladan.zoricic@gmail.com>
2018-01-29 10:47:52 +0100

    000 - Added unit tests for CoordinatesValue class.

---
+++ tests/unit/src/Common/CoordinatesValueTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use G4\DataMapper\Common\CoordinatesValue;
+
+class CoordinatesValueTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var CoordinatesValue
+     */
+    private $coordinates;
+
+    protected function setUp()
+    {
+        $this->coordinates = new CoordinatesValue(46.100376, 19.667587, 100);
+    }
+
+    protected function tearDown()
+    {
+        $this->coordinates = null;
+    }
+
+    public function testFormat()
+    {
+        $expectedArray = [
+            'fq'     => '{!geofilt}',
+            'sfield' => 'location',
+            'pt'     => '46.100376,19.667587',
+            'd'      => '100',
+        ];
+
+        $this->assertEquals($expectedArray, $this->coordinates->format());
+    }
+
+}
