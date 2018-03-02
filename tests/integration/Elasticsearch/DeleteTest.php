a0add17244a7ce80f8fede9ffb310c4d021a1459
Vladan <vladan.zoricic@gmail.com>
2018-03-02 11:58:52 +0100

    000 - Added delete integration test.

---
+++ tests/integration/Elasticsearch/DeleteTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace G4\DataMapper\Test\Integration\Elasticsearch;
+
+class DeleteTest extends TestCase
+{
+    public function testDelete()
+    {
+        $this->makeMapper()->insert($this->makeMapping());
+
+        $this->makeMapper()->delete($this->makeIdentityById());
+
+        sleep(2);
+
+        $rawData = $this->makeMapper()->find($this->makeIdentityById());
+
+        $this->assertEquals(0, $rawData->count());
+        $this->assertEquals(null, $rawData->getOne());
+    }
+
+    public function getCollectionName()
+    {
+        return 'integration_test';
+    }
+}
