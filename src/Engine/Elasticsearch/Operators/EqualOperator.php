7f6d236f043a602802d3870a3867d4485eb1a497
Vladan <vladan.zoricic@gmail.com>
2018-03-06 12:53:12 +0100

    000 - Added separate class for Equal Operator.

---
+++ src/Engine/Elasticsearch/Operators/EqualOperator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace G4\DataMapper\Engine\Elasticsearch\Operators;
+
+use G4\DataMapper\Common\QueryOperatorInterface;
+use G4\DataMapper\Common\SingleValue;
+use G4\DataMapper\Common\QueryConnector;
+
+class EqualOperator implements QueryOperatorInterface
+{
+    public $name;
+
+    public $value;
+
+    public function __construct($name, SingleValue $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function format()
+    {
+        [
+            QueryConnector::MATCH =>
+                [
+                    $this->name => $this->value
+                ]
+        ];
+    }
+}
