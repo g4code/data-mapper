2d52f5272e1721c3277ae8a9808c42d30bec658b
Vladan <vladan.zoricic@gmail.com>
2018-03-07 15:39:30 +0100

    000 - Added class which structures search geodist parameters suitable for Elasticsearch.

---
+++ src/Engine/Elasticsearch/ElasticsearchGeodistFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace G4\DataMapper\Engine\Elasticsearch;
+
+class ElasticsearchGeodistFormatter
+{
+    private $identity;
+
+    public function __construct(ElasticSearchIdentityInterface $identity)
+    {
+        $this->identity = $identity;
+    }
+
+    public function format()
+    {
+        return $this->identity->hasCoordinates() ? $this->formatData() : [];
+    }
+
+    private function formatData()
+    {
+        return [
+            'geo_distance' => [
+                'distance'     => $this->identity->getCoordinates()->getDistance() . 'km',
+                'pin.location' => [
+                    'lon' => $this->identity->getCoordinates()->getLongitude(),
+                    'lat' => $this->identity->getCoordinates()->getLatitude(),
+                ],
+            ],
+        ];
+    }
+}
