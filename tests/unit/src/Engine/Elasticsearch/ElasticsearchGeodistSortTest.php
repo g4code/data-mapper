be0bc4287f30ec8b105e3856bb564330a17cce01
Vladan <vladan.zoricic@gmail.com>
2018-03-08 10:14:17 +0100

    000 - Added unit test for Elasticsearch geodist sort class.

---
+++ tests/unit/src/Engine/Elasticsearch/ElasticsearchGeodistSortTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use G4\DataMapper\Engine\Elasticsearch\ElasticsearchIdentity;
+use G4\DataMapper\Engine\Elasticsearch\ElasticsearchGeodistSort;
+
+class ElasticsearchGeodistSortTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ElasticsearchIdentity
+     */
+    private $identity;
+
+    protected function setUp()
+    {
+        $this->identity = new ElasticsearchIdentity();
+    }
+
+    public function testSortWithGeodistParameters()
+    {
+        $this->identity->geodist(10, 5, 100);
+
+        $geodistFormatter = new ElasticsearchGeodistSort($this->identity);
+
+        $expectedArray = [
+            '_geo_distance' => [
+                'location' => [
+                    'lat' => '5',
+                    'lon' => '10',
+                ],
+                'order' => 'asc',
+                'unit'  => 'km',
+                'distance_type' => 'plane',
+            ],
+        ];
+
+        $this->assertEquals($expectedArray, $geodistFormatter->sort());
+    }
+
+    public function testSortWithEmptyGeodistParameters()
+    {
+        $geodistFormatter = new ElasticsearchGeodistSort($this->identity);
+
+        $this->assertEquals([], $geodistFormatter->sort());
+    }
+}
